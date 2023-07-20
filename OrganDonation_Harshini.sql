0e1bad83eea70eaa06ee27de00decb0984782342
Harshini7802 <lingalaharshini@gmail.com>
2023-07-20 09:13:21 +0000

    query commit

--- OrganDonation_Harshini.sql
+++ OrganDonation_Harshini.sql
@@ -10,14 +10,9 @@ SELECT COUNT(ID) AS "COUNT OF NO OF BLOOD BANK" , STATE FROM BLOOD_BANK GROUP BY
 
 -- (2) Write a sql querry to list out the blood banks in Andrapradesh state 
 SELECT "Blood Bank Name" from BLOOD_BANK WHERE STATE = 'Andhra Pradesh';
--- Just cross checking 
-SELECT COUNT ("Blood Bank Name") from BLOOD_BANK WHERE STATE = 'Andhra Pradesh';
-
 
 -- (3) Write a sql query to find  total blood banks in data set 
 SELECT DISTINCT "Blood Bank Name" from BLOOD_BANK;
---Just cross checking
-SELECT DISTINCT COUNT("Blood Bank Name") from BLOOD_BANK;
 
 -- (4) Write a sql query to display the first 5 blood bank's names in ascending order 
 SELECT DISTINCT "Blood Bank Name" FROM BLOOD_BANK ORDER BY "Blood Bank Name"  FETCH NEXT 5 ROWS ONLY;
