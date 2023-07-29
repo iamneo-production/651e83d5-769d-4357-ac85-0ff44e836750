27edbd418bfd57dfcd4042b9b92d9780e428dbd2
Harshini7802 <lingalaharshini@gmail.com>
2023-07-29 09:16:09 +0000

    final commit

--- OrganDonation_Harshini.sql
+++ OrganDonation_Harshini.sql
@@ -9,12 +9,10 @@ FROM BLOOD_BANK GROUP BY STATE;
 
 
 --Write a sql querry to list out the blood banks in Andrapradesh state 
-SELECT "Blood Bank Name" 
-FROM BLOOD_BANK WHERE STATE = 'Andhra Pradesh';
+SELECT "Blood Bank Name"  FROM BLOOD_BANK WHERE STATE = 'Andhra Pradesh';
 
 --Write a sql query to find  total blood banks in data set 
-SELECT DISTINCT "Blood Bank Name" 
-FROM BLOOD_BANK;
+SELECT DISTINCT "Blood Bank Name"  FROM BLOOD_BANK;
 
 --Write a sql query to display the first 5 blood bank's names in ascending order 
 SELECT "Blood Bank Name" FROM BLOOD_BANK ORDER BY "Blood Bank Name" FETCH FIRST 5 ROWS ONLY;
