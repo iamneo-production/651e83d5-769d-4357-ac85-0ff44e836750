403d02ce6f1f1a38c0207803f033bb1c29bc09d0
HariTammu <tammuhariprasad99@gmail.com>
2023-07-31 11:18:08 +0000

    final code

--- HariTammu.sql
+++ HariTammu.sql
@@ -14,4 +14,5 @@ SELECT DISTINCT "Blood Bank Name"  FROM BLOOD_BANK;
 SELECT "Blood Bank Name" FROM BLOOD_BANK ORDER BY "Blood Bank Name" FETCH FIRST 5 ROWS ONLY;
 
 --Write a sql query to list the blood banks name in alphabetical order 
-SELECT "Blood Bank Name" FROM BLOOD_BANK ORDER BY ASC "Blood Bank Name";
+SELECT DISTINCT "Blood Bank Name" FROM BLOOD_BANK ORDER BY  "Blood Bank Name" asc;
+
